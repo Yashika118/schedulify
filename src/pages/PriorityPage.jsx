b75e044abc5ee88dddb3395acdc2eddb543642a0
Yashika118 <kumariyashika78@gmail.com>
2024-12-21 20:46:53 +0530

    priority page is completed

--- src/pages/PriorityPage.jsx
+++ src/pages/PriorityPage.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
+import InputForm from '../components/InputForm';
+import ResultsTable from '../components/ResultsTable';
+import { priority } from '../Algorithms/PriorityAlgo';
 
 const PriorityPage = () => {
+  const [results, setResults] = useState([]);
+
+  const handleFormSubmit = (processes) => {
+    // console.log("Submitted Processes : ", processes);
+    const output = priority(processes);
+    setResults(output);
+  };
+
   return (
     <div className='min-h-screen bg-yellow-400'>
+      {/* navbar */}
       <div className='h-full flex flex-col bg-yellow-400 items-center'>
         <Navbar title="PRIORITY SCHEDULING" />
-       
-       
+
+        <div className='my-5'>
+
+          <InputForm onSubmit={handleFormSubmit} />
+
+          {
+            results.length > 0 && (
+              <>
+                <ResultsTable results={results}/>
+                {/* <GanttChart results={results}/> */}
+              </>
+            ) 
+          }
+
+
+
+        </div>
+
       </div>
+
+
+
     </div>
   )
 }
